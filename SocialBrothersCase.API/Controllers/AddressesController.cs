63faf3638b535f33d29533d1b033a6dc82767cf0
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2023-05-16 10:52:55 +0200

    Swagger doc update

--- SocialBrothersCase.API/Controllers/AddressesController.cs
+++ SocialBrothersCase.API/Controllers/AddressesController.cs
@@ -1,5 +1,4 @@
 using MediatR;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using SocialBrothersCase.AddressDomain;
 using SocialBrothersCase.API.Commands;
@@ -18,6 +17,14 @@ namespace SocialBrothersCase.API.Controllers
             this._mediator = mediator;
         }
         
+        /// <summary>
+        /// Returns all addresses
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="descending"></param>
+        /// <returns>List of addresses</returns>
+        /// <response code="200">Returns array of addresses</response>
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] string? filter, [FromQuery] string? orderBy, [FromQuery] bool descending)
         {
@@ -26,6 +33,13 @@ namespace SocialBrothersCase.API.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Returns address with the given Id
+        /// </summary>
+        /// <param name="addressId"></param>
+        /// <returns>Address</returns>
+        /// <response code="200">Returns address when found</response>
+        /// <response code="404">Returns 404 not found when address not found</response>
         [HttpGet("{addressId:guid}")]
         public async Task<IActionResult> GetById([FromRoute] Guid addressId)
         {
@@ -35,6 +49,13 @@ namespace SocialBrothersCase.API.Controllers
             return result != null ? Ok(result) : NotFound();
         }
 
+        /// <summary>
+        /// Inserts an address
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns>Newly created address</returns>
+        /// <response code="201">Returns address when created</response>
+        /// <response code="400">Bad request if given address model is not valid</response>
         [HttpPost]
         public async Task<IActionResult> CreateAddress([FromBody] Address address)
         {
@@ -44,6 +65,14 @@ namespace SocialBrothersCase.API.Controllers
             return CreatedAtAction(nameof(GetById), new {addressId = result.Id}, result);
         }
 
+        /// <summary>
+        /// Updates an address
+        /// </summary>
+        /// <param name="addressId"></param>
+        /// <param name="address"></param>
+        /// <returns>Updated address</returns>
+        /// <response code="200">When update successful</response>
+        /// <response code="400">Bad request if given address model is not valid</response>
         [HttpPut("{addressId:guid}")]
         public async Task<IActionResult> EditAddress([FromRoute] Guid addressId, [FromBody] Address address)
         {
@@ -53,6 +82,13 @@ namespace SocialBrothersCase.API.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Deletes an address
+        /// </summary>
+        /// <param name="addressId"></param>
+        /// <returns></returns>
+        /// <response code="204">When deletion successful</response>
+        /// <response code="404">When given id doesn't exist</response>
         [HttpDelete("{addressId:guid}")]
         public async Task<IActionResult> DeleteAddress([FromRoute] Guid addressId)
         {
@@ -62,6 +98,14 @@ namespace SocialBrothersCase.API.Controllers
             return deleted ? NoContent() : NotFound();
         }
 
+        /// <summary>
+        /// Calculates the distance in KM's between two given addresses (id's)
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns>Distance in KM's</returns>
+        /// <response code="200">When distance calculated</response>
+        /// <response code="404">When given ids don't exist</response>
         [HttpGet("distance")]
         public async Task<IActionResult> GetDistanceBetweenAddresses([FromQuery] Guid from, [FromQuery] Guid to)
         {
